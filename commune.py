7db8a8a2696732b3d3b85923b07ce8ffbce69bff
Oystery <wonghouseng@Wongde-MacBook-Air.local>
2019-02-12 09:23:59 -0800

    bug fix and update

--- commune.py
+++ commune.py
@@ -10,28 +10,27 @@ def remove_tags(text):
 
 def findstuff(url):
 #url = input('url: ')
+    t = time.time()
     html = requests.get(url).text
     soup = BeautifulSoup(html,'html.parser')
     address = soup.find('div',attrs={'class':"view-address"}).getText()
     dic = {'\r':'','\n':'','\xa0':'',' ':''}
     address = replace_all(address,dic)
-    print(address)
+    print(f'address: {address}')
 
     #region
-    #region = soup.find('th',text='地區:')
-    #region = soup.find('td',attrs={'class':"td2"})
-    #print(region)
+    tabletags = [tag for tag in soup('td',attrs={'class':"td2"})]
+    region = remove_tags(str(tabletags[1]))
+    print(f'region: {region}')
 
     #name
     name = soup.find('title',text=True).get_text(strip=True).split('-')[0]
-    print(name)
+    print(f'name: {name}')
 
     #price(done)
-    t = time.time()
     pricexml = soup('div',attrs={'class':"view-price-div view-price-red"})
     price = re.findall(r'[0-9]+',str(pricexml))[0]
-    print(price)
-    print(time.time()-t)
+    print(f'price: {price}')
     #for price in soup('div',attrs={'class':"view-price-div view-price-red"}):
     #    prices = re.findall(r'[0-9]+',str(price))
     #    if len(prices)>0:
@@ -46,6 +45,7 @@ def findstuff(url):
             print(f'''lat = {x.split(', ')[0]}''')
             print(f'''long = {x.split(', ')[1]}''')
             #print(str(location[0]))
+    print(time.time()-t)
 
 #address(done)
 def replace_all(text, dic):
@@ -54,12 +54,15 @@ def replace_all(text, dic):
     return text
 
 def main():
-    #with open('urlsTaipa.txt','r') as f:
+    #with open('urlsMacau.txt') as f:
     #    urls = [url.rstrip() for url in f]
     #    for url in urls:
-    #        findstuff(url)
-    #        print(url)
-    findstuff('http://www.malimalihome.net/residential/2351203')
+    #        try:findstuff(url)
+    #        except KeyboardInterrupt:
+    #            raise
+    #        except:continue
+
+    findstuff('http://www.malimalihome.net/residential/2914742')
 
 if __name__ == '__main__':
     main()
--- database.py
+++ database.py
@@ -7,34 +7,38 @@ TAG_RE = re.compile(r'<[^>]+>')
 def remove_tags(text):
     return TAG_RE.sub('',text)
 
-def findstuff(soup):
+def findstuff(url):
+    html = requests.get(url).text
+    soup = BeautifulSoup(html,'html.parser')
     address = soup.find('div',attrs={'class':"view-address"}).getText()
     dic = {'\r':'','\n':'','\xa0':'',' ':''}
     address = replace_all(address,dic)  #return
     #print(address)
 
-    #title
+    #region
+    tabletags = [tag for tag in soup('td',attrs={'class':"td2"})]
+    region = remove_tags(str(tabletags[1]))
+    #print(f'region: {region}')
+
+    #name
     name = soup.find('title',text=True).get_text(strip=True).split('-')[0]  #return
     #print(name)
 
     #price(done)
-    #for price in soup('span',attrs={'class':"price"}):
-    #    prices = re.findall(r'[0-9]+',str(price))
-    #    if len(prices)>0:
-    #        #print('售價 {} 萬'.format(prices[0]))
-    #        price = prices[0]  #return
     pricexml = soup('div',attrs={'class':"view-price-div view-price-red"})
     price = re.findall(r'[0-9]+',str(pricexml))[0]
 
     #location(done)
-    coordinates = None
+    coordinates = '0'
+    lat = '0'
+    long = '0'
     for x in soup('script',attrs={'type':"text/javascript"},text=True):
         location = re.findall('google.maps.LatLng\(([0-9]*.[0-9]*, [0-9]*.[0-9]*)\)',str(x))
         if len(location)>0:
             coordinates = location[0]
             lat = coordinates.split(', ')[0]
             long = coordinates.split(', ')[1]
-    return (name,address,price,lat,long)
+    return (name,address,region,price,coordinates,lat,long)
 #address(done)
 def replace_all(text, dic):
     for i, j in dic.items():
@@ -42,25 +46,25 @@ def replace_all(text, dic):
     return text
 
 def main():
-    tic = 0
-    with open('urlsColoane.txt','r') as f:
+    with open('urlsMacau.txt','r') as f:
         conn = sqlite3.connect('database.db')
         cur = conn.cursor()
         #cur.execute('''DROP TABLE IF EXISTS Housing''')
         urls = [url.rstrip() for url in f]
         for url in urls:
-            response = requests.get(url)
-            html = response.text
-            soup = BeautifulSoup(html,'html.parser')
-            try:(name,address,price,lat,long) = findstuff(soup)
-            except:continue
+            #try:
+            (name,address,region,price,coordinates,lat,long) = findstuff(url)
+            print(url,name,address,region,price,coordinates,lat,long)
+            #except KeyboardInterrupt:
+            #    raise
+            #except:
+            #    print(f'skipped {url}')
+            #    continue
             cur.execute('''CREATE TABLE IF NOT EXISTS Housing (
-            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, url TEXT UNIQUE,name TEXT,address TEXT,price INTEGER, coordinates INTEGER
-            )''')
-            cur.execute('''INSERT OR IGNORE INTO Housing (url,name,address,price,lat,long) VALUES (?,?,?,?,?,?)''',(url,name,address,price,lat,long))
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, url TEXT UNIQUE,name TEXT,address TEXT,region TEXT,price INTEGER, coordinates INTEGER
+            , lat, long)''')
+            cur.execute('''INSERT OR IGNORE INTO Housing (url,name,address,region,price,coordinates,lat,long) VALUES (?,?,?,?,?,?,?,?)''',(url,name,address,region,price,coordinates,lat,long))
             conn.commit()
-            tic += 1
-            print(url,name,address,price,lat,long)
             #if tic >= 5:
             #    inp = input('committed')
             #    if inp == '':
