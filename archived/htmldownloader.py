56bb7140fc4e96dca36f12d439412c4da8c39d00
Oystery <wonghouseng@oysteryio.local>
2019-02-21 08:56:27 -0800

    delete

--- archived/htmldownloader.py
+++
@@ -1,4 +0,0 @@
-import requests
-html = requests.get('http://www.malimalihome.net/residential/2935217').text
-with open('html4.txt','w+') as f:
-    f.write(html)
--- archived/title.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-html = requests.get('http://www.malimalihome.net/residential/2939440').text
-soup = BeautifulSoup(html,'html.parser')
-#title
-title = soup.find('title',text=True).get_text(strip=True).split('-')[0]
-print(title)
